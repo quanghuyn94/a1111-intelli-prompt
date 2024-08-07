29add041dd0baedc3a05f981d12c830e11f524e5
6789zkm <quanghuyn94@gmail.com>
2024-08-07 14:00:14 +0700

    Remove unnecessary things

--- javascript/intelliMain.js
+++ javascript/intelliMain.js
@@ -3,8 +3,7 @@ var setup = true
 onUiUpdate(async () => {
     if (!setup) return;
     if (Object.keys(opts).length === 0) return;
-    WORD_PADDING = opts['intelli_word_padding']
-    HISTORY_LENGTH = opts['intelli_max_history_length']
+    
     try {
         var txt2img_prompt_autocomplete = new AutoCompleteField(gradioApp().querySelector("#txt2img_prompt > label"), (k, textarea, autoCompleteContainer) => {
             suggestionWordAsync(k, textarea, autoCompleteContainer).then();
